8f11baadbd1bb437a7240b1f6b62d880b15c3b5d
anshugahlaut <anshugahlaut1999@gmail.com>
2021-11-08 12:16:52 +0530

    ServletAttributes

---
+++ ServletAttributes/src/main/java/com/nt/servlet/Servlet3.java
@@ -0,0 +1,45 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/s3url")
+public class Servlet3 extends HttpServlet {
+	
+		@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	
+		//get PrintWriter
+		PrintWriter pw=res.getWriter();
+		//set response content type
+		res.setContentType("text/html");
+		//read and display request attribute values
+		pw.println("<b>Servlet3:: attr1(req) value::"+req.getAttribute("attr1")+"</b>");		
+	
+		//read and display ses attribute values
+		HttpSession ses=req.getSession();
+		pw.println("<br><b>Servlet3:: attr2(req) value::"+ses.getAttribute("attr2")+"</b>");
+		
+		//read and disply ServletContext attribute values
+		ServletContext sc=getServletContext();
+		pw.println("<br><b>Servlet3:: attr3(sc) value::"+sc.getAttribute("attr3")+"</b>");
+				
+				
+	}//doGet(-,-)
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req,res);
+	}//doPost(-,-)
+
+	
+}//class
